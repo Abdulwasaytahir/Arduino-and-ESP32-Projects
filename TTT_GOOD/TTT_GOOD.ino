f05331248d1ef8421e1ec93ab2a63d358180698b
Abdulwasaytahir <abdulwasaytahir01@gmail.com>
2026-07-27 00:19:31 +0500

    Update API key to placeholder value

--- TTT_GOOD/TTT_GOOD.ino
+++ TTT_GOOD/TTT_GOOD.ino
@@ -5,7 +5,7 @@
 
 const char* ssid = "home";
 const char* password = "6strings";
-String apiKey = "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"; 
+String apiKey = "Your API Key"; 
 
 WebServer server(80);
 
